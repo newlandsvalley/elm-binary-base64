6f2318bb0273c5cd9304206c544dc8bf7b5106e2
John Watson <john.watson@gmx.co.uk>
2018-02-12 15:08:43 +0000

    Add StackSafety tests
    Not sure how useful these are, but it does at least show that the stack isn't blown when encoding a list of 50K integers.  Hopefully this can serve as a placeholder for more exacting tests in the future.

---
+++ tests/Test/StackSafety.elm
@@ -0,0 +1,24 @@
+module Test.StackSafety exposing (..)
+
+import Test exposing (..)
+import Expect exposing (..)
+import String
+import BinaryBase64 exposing (encode)
+
+
+-- just ensure that the tests actually run
+-- I'm not sure of the behavious of elm-test if there's a stack issue
+-- When set to 100K, the test seems to hang
+
+
+tests : Test
+tests =
+    describe "stack safety tests"
+        [ test "encode big input" <|
+            \_ ->
+                let
+                    bigInput =
+                        List.repeat 50000 0x00
+                in
+                    Expect.greaterThan 0 (String.length (encode bigInput))
+        ]
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,10 +2,12 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Test.RoundTrip exposing (tests)
+import Test.StackSafety exposing (tests)
 
 
 all : Test
 all =
     concat
         [ Test.RoundTrip.tests
+        , Test.StackSafety.tests
         ]
